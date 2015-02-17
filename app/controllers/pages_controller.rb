0469451ecd086a7ace3e97b0d1af6203b3e68473
Jason Benn <jasoncbenn@gmail.com>
2015-02-17 08:05:14 -0800

    Extract to layout, disable other links, active link styling.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,21 @@
 class PagesController < ApplicationController
   def bio
+    @active = 'bio'
   end
 
   def platform
+    @active = 'platform'
   end
 
   def issues
+    @active = 'issues'
   end
 
   def donate
+    @active = 'donate'
   end
 
   def share
+    @active = 'share'
   end
 end
