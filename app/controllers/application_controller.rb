840dbaf0f322b0fef1101f5e599fa79cda102c77
Jason Benn <jasoncbenn@gmail.com>
2015-03-02 14:50:34 -0800

    Helpers are for views, application controller is for controller methods.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def email(options)
+    defaults = {
+      to: 'jasoncbenn@gmail.com', 
+      from: 'bugs@friendsofscottcameron.com',
+      subject: 'Notification: friendsofscottcameron',
+      body: 'Check friendsofscottcameron.com Heroku logs!'
+    }
+
+    Pony.mail(defaults.merge(options))
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,2 @@
 module ApplicationHelper
-  def email(options)
-    defaults = {
-      to: 'jasoncbenn@gmail.com', 
-      from: 'bugs@friendsofscottcameron.com',
-      subject: 'Notification: friendsofscottcameron',
-      body: 'Check friendsofscottcameron.com Heroku logs!'
-    }
-
-    Pony.mail(defaults.merge(options))
-  end
 end
