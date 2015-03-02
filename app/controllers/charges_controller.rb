00b21850c345da7256613c01790dd39be15e20f6
Jason Benn <jasoncbenn@gmail.com>
2015-03-02 14:38:59 -0800

    Oh, right. =>

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -18,19 +18,19 @@ class ChargesController < ApplicationController
     email(subject: "Successful #{amount} donation to friendsofscottcameron!", body: "#{params[:email]} donated #{amount}.\n\n#{params.inspect}")
     render json: { success: true, message: "Thank you for your donation of #{amount}" }
 
-  rescue Stripe:CardError: e
+  rescue Stripe::CardError => e
     email(subject: 'FriendofScottCameron bug: CardError!', body: "Card was declined.")
     render json: { success: false, message: "Card was declined." }
-  rescue Stripe:InvalidRequestError: e
+  rescue Stripe::InvalidRequestError => e
     email(subject: 'FriendofScottCameron bug: InvalidRequestError!', body: "Invalid parameters were supplied to Stripe's API.")
     render json: { success: false, message: "Invalid parameters were supplied to Stripe's API." }
-  rescue Stripe:AuthenticationError: e
+  rescue Stripe::AuthenticationError => e
     email(subject: 'FriendofScottCameron bug: AuthenticationError!', body: "Authentication with Stripe's API failed (maybe you changed API keys recently?).")
     render json: { success: false, message: "Authentication with Stripe's API failed (maybe you changed API keys recently?)." }
-  rescue Stripe:APIConnectionError: e
+  rescue Stripe::APIConnectionError => e
     email(subject: 'FriendofScottCameron bug: APIConnectionError!', body: "Network communication with Stripe failed.")
     render json: { success: false, message: "Network communication with Stripe failed." }
-  rescue Stripe:StripeError: e
+  rescue Stripe::StripeError => e
     email(subject: 'FriendofScottCameron bug: StripeError!', body: "An error has prevented your donation from succeeding.")
     render json: { success: false, message: "An error has prevented your donation from succeeding." }
   end
