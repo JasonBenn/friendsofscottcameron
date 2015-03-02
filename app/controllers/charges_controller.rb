3dff70e63da1d7e47001732fcd2fcc64e3331cf0
Jason Benn <jasoncbenn@gmail.com>
2015-03-02 14:35:54 -0800

    Update donate form. Add email triggers. Add sweetAlert styles.

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -4,35 +4,34 @@ class ChargesController < ApplicationController
     amount = params[:amount]
 
     customer = Stripe::Customer.create(
-      :email => params[:email],
-      :card  => params[:id]
+      email: params[:email],
+      card: params[:id]
     )
 
     charge = Stripe::Charge.create(
-      :customer    => customer.id,
-      :amount      => amount,
-      :description => 'Donation to friendsofscottcameron.com',
-      :currency    => 'usd'
+      customer: customer.id,
+      amount: amount,
+      description: 'Donation to friendsofscottcameron.com',
+      currency: 'usd'
     )
 
+    email(subject: "Successful #{amount} donation to friendsofscottcameron!", body: "#{params[:email]} donated #{amount}.\n\n#{params.inspect}")
     render json: { success: true, message: "Thank you for your donation of #{amount}" }
 
-  rescue Stripe::CardError => e
-    # Since it's a decline, Stripe::CardError will be caught
+  rescue Stripe:CardError: e
+    email(subject: 'FriendofScottCameron bug: CardError!', body: "Card was declined.")
     render json: { success: false, message: "Card was declined." }
-  rescue Stripe::InvalidRequestError => e
-    # Invalid parameters were supplied to Stripe's API
+  rescue Stripe:InvalidRequestError: e
+    email(subject: 'FriendofScottCameron bug: InvalidRequestError!', body: "Invalid parameters were supplied to Stripe's API.")
     render json: { success: false, message: "Invalid parameters were supplied to Stripe's API." }
-  rescue Stripe::AuthenticationError => e
-    # Authentication with Stripe's API failed
-    # (maybe you changed API keys recently)
-    render json: { success: false, message: "Authentication with Stripe's API failed." }
-  rescue Stripe::APIConnectionError => e
-    # Network communication with Stripe failed
+  rescue Stripe:AuthenticationError: e
+    email(subject: 'FriendofScottCameron bug: AuthenticationError!', body: "Authentication with Stripe's API failed (maybe you changed API keys recently?).")
+    render json: { success: false, message: "Authentication with Stripe's API failed (maybe you changed API keys recently?)." }
+  rescue Stripe:APIConnectionError: e
+    email(subject: 'FriendofScottCameron bug: APIConnectionError!', body: "Network communication with Stripe failed.")
     render json: { success: false, message: "Network communication with Stripe failed." }
-  rescue Stripe::StripeError => e
-    # Display a very generic error to the user, and maybe send
-    # yourself an email
+  rescue Stripe:StripeError: e
+    email(subject: 'FriendofScottCameron bug: StripeError!', body: "An error has prevented your donation from succeeding.")
     render json: { success: false, message: "An error has prevented your donation from succeeding." }
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def email(options)
+    defaults = {
+      to: 'jasoncbenn@gmail.com', 
+      from: 'bugs@friendsofscottcameron.com',
+      subject: 'Notification: friendsofscottcameron',
+      body: 'Check friendsofscottcameron.com Heroku logs!'
+    }
+
+    Pony.mail(defaults.merge(options))
+  end
 end
